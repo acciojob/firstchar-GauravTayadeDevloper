0044625b646878f3a0c4a434c55f1e7d30e8c9ed
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-10-04 00:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+
 function firstChar(text) {
-  // your code here
+  // Remove leading and trailing spaces
+  let v = text.trim();
+
+  // If empty after trimming, return empty string
+  if (v.length === 0) {
+    return "";
+  }
+
+  // Otherwise return first character
+  return v[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
